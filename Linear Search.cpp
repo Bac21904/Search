f38451a06eafadc3b38623f5345e8def56a1a101
Bac21904 <148106725+Bac21904@users.noreply.github.com>
2023-12-11 22:30:03 +0700

    Linear Search.cpp

--- Linear Search.cpp
+++ Linear Search.cpp
@@ -13,11 +13,12 @@ int linearSearch(int arr[], int target, int n)
 int main() 
 {
     int arr[100000];
-    int n;
-    cin>>n;
+    int n=100000;
     for(int i=0;i<n;i++) arr[i]=i;
-    int target = 9999;
+    int target;
+    cout<<"Nhap gia tri can tim: ";
+    cin>>target;
     int res = linearSearch(arr, target, n);    
-    cout << "The result of linear search = " << res << endl;
+    cout << "Phan tu can tim o vi tri so: " << res << endl;
     return 0;
 }
