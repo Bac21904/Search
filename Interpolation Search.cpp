ce63f84aea94fba0d241bfdb6d11176763977cc2
Bac21904 <148106725+Bac21904@users.noreply.github.com>
2023-12-16 21:35:00 +0700

    Add files via upload

---
+++ Interpolation Search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int InterpolationSearch( int arr[], int n, int x)
+{
+	int l=0, r=n-1;
+	while (l<=r && arr[l]<=x && arr[r]>=x)
+	{
+		if(l==r)
+		{
+			if(arr[l]==x)
+				return l;
+			return -1;
+		}
+		l++;
+		r--;
+	}
+}
+int main()
+{
+	int n;
+	cout<<"nhap so phan tu cua mang ";
+	cin>>n;
+	int arr[100000];
+	for(int i=0;i<n;i++)
+		arr[i]=i+1;
+	int x;
+	cout<<"nhap gia tri can tim ";
+	cin>>x;
+	int tg=InterpolationSearch(arr, n, x);
+	if(tg==-1) cout<<"phan tu can tim khong o trong mang";
+	else cout<<"phan tu can tim o vi tri thu "<<tg;
+	return 0;
+}
